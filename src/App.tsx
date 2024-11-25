6c1467680ba1f19ccfc12c1aa7f7ce057ffbaea8
Adrianlsq2000 <asalamea@espol.edu.ec>
2024-11-24 20:57:56 -0500

    Componente MUI Paper y Typograph

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import IndicatorWeather from './components/IndicatorWeather';
 import './App.css'
 
 // Grid version 2
@@ -9,10 +9,18 @@ function App() {
     <Grid container spacing={5}>
 
        	  {/* Indicadores */}
-           <Grid size={{xs:12, xl:3}}>Elemento: Indicador 1</Grid>
-           <Grid size={{xs:12, xl:3}}>Elemento: Indicador 2</Grid>
-           <Grid size={{xs:12, xl:3}}>Elemento: Indicador 3</Grid>
-           <Grid size={{xs:12, xl:3}}>Elemento: Indicador 4</Grid>
+           <Grid size={{xs:12, xl:3}}>
+           <IndicatorWeather title={'IndicatorWeather 1'} subtitle={'Unidad 1'} value={1.23} /> 
+          </Grid>
+           <Grid size={{xs:12, xl:3}}>
+           <IndicatorWeather title={'IndicatorWeather 2'} subtitle={'Unidad 2'} value={3.12} />
+          </Grid>
+           <Grid size={{xs:12, xl:3}}>
+           <IndicatorWeather title={'IndicatorWeather 3'} subtitle={'Unidad 3'} value={2.31} />
+          </Grid>
+           <Grid size={{xs:12, xl:3}}>
+           <IndicatorWeather title={'IndicatorWeather 4'} subtitle={'Unidad 4'} value={3.21} />
+          </Grid>
 		      
            {/* Tabla */}
            <Grid size={{xs:12, xl:8}}>Elemento: Tabla</Grid>
