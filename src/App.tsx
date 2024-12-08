9fdbc81e8f00ab7ec3740423d40b74e473931be1
Adrianlsq2000 <asalamea@espol.edu.ec>
2024-12-07 22:20:48 -0500

    Cosas

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,72 @@ import ControlWeather from './components/ControlWeather';
 import Grid from '@mui/material/Grid2'
 import TableWeather from './components/TableWeather';
 import LineChartWeather from './components/LineChartWeather';
+{/* Hooks */ }
+import { useEffect, useState } from 'react';
+import Item from './interface/Item';
+interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
+
 function App() {
+  {/* Variable de estado y función de actualización */ }
+  let [indicators, setIndicators] = useState<Indicator[]>([])
+  {/*Variables de estado y funcion de actualización */}
+  const [items, setItems] = useState<Item[]> ([]); 
+  {/* Hook: useEffect */ }
+  useEffect(() => {
+
+    let request = async () => {
+
+      {/* Request */ }
+      let API_KEY = "65b472dd5feb7fe34312306364b9926e"
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      let savedTextXML = await response.text();
+
+      {/* XML Parser */ }
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+      {/* Arreglo para agregar los resultados */ }
+
+      let dataToIndicators: Indicator[] = new Array<Indicator>();
+
+      {/* 
+             Análisis, extracción y almacenamiento del contenido del XML 
+             en el arreglo de resultados
+         */}
+
+      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name })
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let latitude = location.getAttribute("latitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+      let longitude = location.getAttribute("longitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+      let altitude = location.getAttribute("altitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+      //console.log( dataToIndicators )
+      {/* Modificación de la variable de estado mediante la función de actualización */ }
+      setIndicators(dataToIndicators)
+
+    }
+
+    request();
+
+  }, [])
+
 
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}>
+      {/*<Grid size={{ xs: 12, xl: 3 }}>
         <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
       </Grid>
       <Grid size={{ xs: 12, xl: 3 }}>
@@ -23,7 +82,20 @@ function App() {
       <Grid size={{ xs: 12, xl: 3 }}>
         <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
       </Grid>
-
+        */}
+      {
+        indicators
+          .map(
+            (indicator, idx) => (
+              <Grid key={idx} size={{ xs: 12, /*md*/ xl: 3 }}>
+                <IndicatorWeather
+                  title={indicator["title"]}
+                  subtitle={indicator["subtitle"]}
+                  value={indicator["value"]} />
+              </Grid>
+            )
+          )
+      }
       {/* Tabla */}
       <Grid size={{ xs: 12, xl: 8 }}>
         {/* Grid Anidado */}
---
+++ src/interface/Item.tsx
@@ -0,0 +1,8 @@
+// Interfaz con sus claves tipo string
+export default interface Item{
+    dateStart: String;
+    dateEnd: String;
+    precipitacion: String;
+    humidity:String;
+    clouds: String;
+}
